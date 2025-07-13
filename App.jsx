4ec070792d7a42db4a8930474889605c42b7b2f9
Santyel <119018402+Santyel@users.noreply.github.com>
2025-07-13 09:05:19 +0200

    Vault základ

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'monospace' }}>
+      <h1>Vault stránka</h1>
+      <p>Pripoj svoju walletku cez MetaMask</p>
+    </div>
+  )
+}
